4188c1842fcb9f297cc2a6ce6a3bb46a6b116db2
N10511097 <nina.budna@student.poznan.put.pl>
2021-01-25 13:19:25 +0100

    zwracanie liczby eulera

--- euler.py
+++ euler.py
@@ -1,9 +1,17 @@
 def factorial(value):
     x= 1
-    for n in (range(value)):
+    for n in range(value):
         x= x * (n+1)
     return x
 
 print(factorial(0))
 print(factorial(5))
 print(factorial(8))
+
+def euler_from_series(precision):
+    e = 0
+    for n in range(precision+1):
+        e = e + (1/factorial(n))
+    return e
+
+print(euler_from_series(100))
